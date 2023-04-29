53af9d4b26b43913c524137ce1d9659de8ad2781
vlburmistov <s24b_burmistrov@179.ru>
2023-04-29 12:35:04 +0300

    теперь архиватор перемещает файл в /files

--- zip.py
+++ zip.py
@@ -7,3 +7,8 @@ file_name = sorted(os.listdir("files/documents"))[-1]
 
 with zipfile.ZipFile("archive.zip", mode="w") as archive:
     archive.write("files/documents/file_0.pdf")
+
+old_file = 'archive.zip'
+destination_file = 'files/archive.zip'
+
+os.rename(old_file, destination_file)
