09534d006d07cd2ddc89acf3ec6ea152db5a3c78
vlburmistov <s24b_burmistrov@179.ru>
2023-05-12 12:31:51 +0300

    все перестроено на aiogram 3

--- bot.py
+++ bot.py
@@ -1,68 +1,124 @@
+import asyncio
 import subprocess
-import logging
 
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from aiogram.dispatcher.filters import Text
-from aiogram.bot.api import TelegramAPIServer
+from inline_keyboards import ikb
+
+from aiogram import Bot, Dispatcher, F
+from aiogram import types
+from aiogram.types import FSInputFile
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.filters import Command, CommandStart, StateFilter
+from aiogram.filters.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import (CallbackQuery, InlineKeyboardButton,
+                           InlineKeyboardMarkup, Message, PhotoSize)
+from aiogram.client.telegram import TelegramAPIServer
 
 from config import TOKEN
-from keybords import greet_kb
 
-logging.basicConfig(level=logging.INFO)
-local_server = TelegramAPIServer.from_base('http://127.0.0.1:8081/')
-bot = Bot(token=TOKEN, server=local_server)
-dp = Dispatcher(bot)
+session = AiohttpSession(api=TelegramAPIServer.from_base('http://127.0.0.1:8081/'))
+bot = Bot(token=TOKEN, session=session)
+dp = Dispatcher()
+
+HELP_COMMAND = """
+start - начало работы
+help - список команд
+zip - сжать файл/файлы в один архив
+unzip - разархивировать zip архив
+pdf - конвертировать файл/файлы в один pdf файл
+qr - создать qr-code
+"""
+
+storage: MemoryStorage = MemoryStorage()
 
 
-@dp.message_handler(commands=["zip"])
-@dp.message_handler(Text("Сжать файл"))
+# class FSMFillForm(StatesGroup):
+#     # Создаем экземпляры класса State, последовательно
+#     # перечисляя возможные состояния, в которых будет находиться
+#     # бот в разные моменты взаимодейтсвия с пользователем
+#     fill_name = State()  # Состояние ожидания ввода имени
+#     fill_age = State()  # Состояние ожидания ввода возраста
+#     fill_gender = State()  # Состояние ожидания выбора пола
+#     upload_photo = State()  # Состояние ожидания загрузки фото
+#     fill_education = State()  # Состояние ожидания выбора образования
+#     fill_wish_news = State()  # Состояние ожидания выбора получать ли новости
+
+
+@dp.message(Command("qr"))
+async def generate_qr(message: types.Message):
+    await message.answer("Выберейте тип данных, который Вы хотите закодировать.", reply_markup=ikb)
+    # await state.set_state(FSMFillForm.fill_name)
+
+
+# @dp.callback_query()
+# async def get_callback(callback: types.CallbackQuery):
+#     if callback.data == "url":
+#         await callback.message.answer("Отправь ссылку на ресурс.")
+#     if callback.data == "text":
+#         await callback.message.answer("Отправь текстовое сообщение")
+#     if callback.data == "email":
+#         await callback.message.answer("Отправь электронный адрес")
+#     if callback.data == "event":
+#         await callback.message.answer("Отправь место события")
+#         await callback.message.answer("Отправь время и дату события")
+
+
+@dp.message(Command("zip"))
+@dp.message(lambda message: message.text == 'Сжать файл')
 async def doc_info(message: types.Message):
-    await message.answer("Отправьте файл <= 20 Mb (пока)")
+    await message.answer("Отправьте файл/файлы одним сообщением")
 
 
-@dp.message_handler(commands=["pdf"])
-@dp.message_handler(Text("Собрать фото в pdf"))
+@dp.message(Command("pdf"))
+@dp.message(lambda message: message.text == "Собрать фотографии в pdf")
 async def photo_info(message: types.Message):
-    await message.answer("Отправьте файлы")
+    await message.answer("Отправьте фото/фотографии одним сообщением")
 
 
-@dp.message_handler(commands=["unzip"])
-@dp.message_handler(Text("Разархивировать файл"))
+@dp.message(Command("unzip"))
+@dp.message(lambda message: message.text == 'Разархивировать файл')
 async def doc_info(message: types.Message):
-    await message.answer("Отправьте zip архив <= 20 Mb (пока)")
+    await message.answer("Отправьте zip архив")
 
 
-@dp.message_handler(content_types=['photo'])
+@dp.message(lambda message: message.content_type == 'photo')
 async def photo_handler(message: types.Message):
-    print("photo")
     file_id = message.photo[-1].file_id
     file = await bot.get_file(file_id)
+    subprocess.run("python3 pdf.py", shell=True)
+    file = FSInputFile("data/doc.pdf", filename="result.pdf")
+    await bot.send_document(message.from_user.id, file)
 
 
-@dp.message_handler(content_types=['document'])
-async def echo(message: types.Message):
-    print("document")
+@dp.message(lambda message: message.content_type == 'document')
+async def document_hander(message: types.Message):
     file_id = message.document.file_id
     file = await bot.get_file(file_id)
+    subprocess.run("python3 zip.py", shell=True)
+    file = FSInputFile("data/archive.zip", filename="result.zip")
+    await bot.send_document(message.from_user.id, file)
 
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+@dp.message(Command("help"))
+async def help_command(message: types.Message):
+    await message.answer(HELP_COMMAND)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message(Command("start"))
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!", reply_markup=greet_kb)
+    await message.answer("Привет! Отправь команду /help, чтобы узнать что умеет этот бот")
 
 
-@dp.message_handler()
+@dp.message()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
 
 
+async def main():
+    await dp.start_polling(bot)
+
+
 if __name__ == '__main__':
-    # bot.log_out()
-    executor.start_polling(dp)
+    asyncio.run(main())
