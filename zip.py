4974f21cdb5c59d6be8620bd3879507f5852c354
vlburmistov <s24b_burmistrov@179.ru>
2023-05-12 12:32:49 +0300

    сжатие последнего файла

--- zip.py
+++ zip.py
@@ -1,15 +1,15 @@
 import zipfile
 import os
+import config
 
 # https://realpython.com/python-zipfile/
 
-os.chdir("documents")
-file_name = max(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
+files = sorted(os.listdir(f"telegram-bot-api/bin/{config.TOKEN}/documents/"), key=lambda x: int(x[5:-4]))
 
 with zipfile.ZipFile("archive.zip", mode="w") as archive:
-    archive.write(f"{file_name}")
+    archive.write(f"telegram-bot-api/bin/{config.TOKEN}/documents/{files[-1]}", compresslevel=9)
 
-# old_file = 'archive.zip'
-# destination_file = 'files/archive.zip'
-#
-# os.rename(old_file, destination_file)
+old_file = 'archive.zip'
+destination_file = 'data/archive.zip'
+
+os.rename(old_file, destination_file)
